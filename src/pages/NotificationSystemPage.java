1d00fd69a8fe58aaabf6098d9751859600d4ce26
unknown <dunja.glumac97@gmail.com>
2021-10-20 00:43:11 +0200

    NotificationSystemPage added getters and methods

--- src/pages/NotificationSystemPage.java
+++ src/pages/NotificationSystemPage.java
@@ -1,7 +1,10 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class NotificationSystemPage extends BasicPage {
@@ -15,6 +18,20 @@ public class NotificationSystemPage extends BasicPage {
 
 	public NotificationSystemPage(WebDriver driver, WebDriverWait wait, JavascriptExecutor js) {
 		super(driver, wait, js);
+	}
+	
+	//Getters
+	public WebElement getMessage() {
+		return this.driver.findElement(By.xpath("//*[contains(@class, 'alert--success') or contains(@class, 'alert--danger')][contains(@style,'display: block')]"));
+	}
+	
+	//Methods
+	public String messageText() {
+		return this.driver.findElement(By.xpath("//*[contains(@class, 'alert--success') or contains(@class, 'alert--danger')][contains(@style,'display: block')]")).getText();
+	}
+	
+	public void waitTillmessageDisappear() {
+		wait.until(ExpectedConditions.attributeContains(By.xpath("//*[contains(@class, 'alert--success') or contains(@class, 'alert--danger')][contains(@style,'display: block')]"), "style", "display: none"));
 	}
 	
 	
