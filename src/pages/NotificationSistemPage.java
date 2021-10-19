987ccc4518f7d75502ee2836330f6af840dbbe11
unknown <dunja.glumac97@gmail.com>
2021-10-19 20:45:25 +0200

    NotificationSystemPage created

---
+++ src/pages/NotificationSistemPage.java
@@ -0,0 +1,13 @@
+package pages;
+
+public class NotificationSistemPage {
+ 
+//	get metodu za element koji prikazuje poruku
+//			//*[contains(@class, 'alert--success') or contains(@class, 'alert--danger')][contains(@style,'display: block')]
+//	metodu koja vraća poruku koja se nalazi u obaveštenju
+//	metodu koja čeka da obaveštenje nestane
+//			čeka se da element //*[contains(@class, 'system_message')]
+//			za atribut style dobije vrednost  "display: none;"
+
+	
+}
