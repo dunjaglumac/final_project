df7bd2b5434db429b361d08e6e5b7861076bc7cd
unknown <dunja.glumac97@gmail.com>
2021-10-19 20:43:13 +0200

    LoginPage uppdated

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -11,14 +11,6 @@ public class LoginPage extends BasicPage {
 //	get metode za sve potrebne elemente
 //	metodu koja prijavljuje korisnika na sistem - kao parametri se prosleđuju imejl i lozinka
 
-//	Notification Sistem Page:
-//	get metodu za element koji prikazuje poruku
-//			//*[contains(@class, 'alert--success') or contains(@class, 'alert--danger')][contains(@style,'display: block')]
-//	metodu koja vraća poruku koja se nalazi u obaveštenju
-//	metodu koja čeka da obaveštenje nestane
-//		čeka se da element //*[contains(@class, 'system_message')]
-//		za atribut style dobije vrednost  "display: none;"
-
 	public LoginPage(WebDriver driver, WebDriverWait wait, JavascriptExecutor js) {
 		super(driver, wait, js);
 	}
