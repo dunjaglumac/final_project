77c4c17c6a2c180b18f668c11684434b3ed9733c
unknown <dunja.glumac97@gmail.com>
2021-10-19 22:27:26 +0200

    AuthPage updated

--- src/pages/AuthPage.java
+++ src/pages/AuthPage.java
@@ -1,7 +1,34 @@
 package pages;
 
-public class AuthPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AuthPage extends BasicPage {
+
 //	get metode za sve potrebne elemente sa stranice   
 //	metodu koja odjavljuje korisnika sa sistema
 
+	public AuthPage(WebDriver driver, WebDriverWait wait, JavascriptExecutor js) {
+		super(driver, wait, js);
+	}
+	
+	
+	//Getters
+	public WebElement getUserButton() {
+		return driver.findElement(By.xpath("//*[@class='filled ']/a"));
+	}
+	public WebElement getLogoutButton() {
+		return driver.findElement(By.xpath("//*[@class='my-account-dropdown']/ul/li[2]"));
+	} 
+	
+	//Methods
+	public void loggingOut() throws InterruptedException {
+		this.getUserButton().click();
+		Thread.sleep(1000);
+		this.getLogoutButton().click();
+	}
+	
 }
